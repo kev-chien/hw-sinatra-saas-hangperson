66f1d3727fe5248f3a4f6fb161acd433ad89dc52
Kevin Chien <kevinchien17@gmail.com>
2019-02-08 00:14:39 -0800

    passing hangman_game_spec tests

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,7 @@
 class HangpersonGame
 
+  attr_accessor :word, :guesses, :wrong_guesses
+
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -10,6 +12,56 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    # argument validation
+    if letter.nil?
+      raise ArgumentError
+    end
+
+    unless (letter.is_a? String)
+      raise ArgumentError
+    end
+
+    if letter.size != 1
+      raise ArgumentError
+    end
+
+    unless /[A-Za-z]/ =~ letter
+      raise ArgumentError
+    end
+
+    letter.downcase! # case insensitive
+
+    # valid guess
+    if @guesses.include? letter or @wrong_guesses.include? letter
+      return false
+    end
+
+    # check guess
+    correct = @word.include? letter
+    if correct
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+  end
+
+  def word_with_guesses
+    @word.split('').map{|letter| @guesses.include?(letter) ? letter : '-'}.join ''
+  end
+
+  def check_win_or_lose
+    if @word.split('').all?{|letter| @guesses.include?(letter)}
+      :win
+    elsif wrong_guesses.size >= 7
+      :lose
+    else
+      :play
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
